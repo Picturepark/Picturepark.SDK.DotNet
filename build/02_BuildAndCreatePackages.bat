4b22c98fc5296c66fb25f282438de70d175f3b22
Rico Suter <mail@rsuter.com>
2017-02-20 17:51:21 +0100

    Fixed build script

--- build/02_BuildAndCreatePackages.bat
+++ build/02_BuildAndCreatePackages.bat
@@ -3,20 +3,20 @@ mkdir %~dp0/Packages
 
 del "%~dp0/../src/Picturepark.SDK.V1.Contract/project.lock.json"
 dotnet restore "%~dp0/../src/Picturepark.SDK.V1.Contract/" --no-cache
-dotnet pack "%~dp0/../src/Picturepark.SDK.V1.Contract/" --output "../../build/Packages" --configuration Release
+dotnet pack "%~dp0/../src/Picturepark.SDK.V1.Contract/" --output "%~dp0/Packages" --configuration Release
 
 del "%~dp0/../src/Picturepark.SDK.V1/project.lock.json"
 dotnet restore "%~dp0/../src/Picturepark.SDK.V1/" --no-cache
-dotnet pack "%~dp0/../src/Picturepark.SDK.V1/" --output "../../build/Packages" --configuration Release
+dotnet pack "%~dp0/../src/Picturepark.SDK.V1/" --output "%~dp0/Packages" --configuration Release
 
 del "%~dp0/../src/Picturepark.SDK.V1.Localization/project.lock.json"
 dotnet restore "%~dp0/../src/Picturepark.SDK.V1.Localization/" --no-cache
-dotnet pack "%~dp0/../src/Picturepark.SDK.V1.Localization/" --output "../../build/Packages" --configuration Release
+dotnet pack "%~dp0/../src/Picturepark.SDK.V1.Localization/" --output "%~dp0/Packages" --configuration Release
 
 del "%~dp0/../src/Picturepark.SDK.V1.ServiceProvider/project.lock.json"
 dotnet restore "%~dp0/../src/Picturepark.SDK.V1.ServiceProvider/" --no-cache
-dotnet pack "%~dp0/../src/Picturepark.SDK.V1.ServiceProvider/" --output "../../build/Packages" --configuration Release
+dotnet pack "%~dp0/../src/Picturepark.SDK.V1.ServiceProvider/" --output "%~dp0/Packages" --configuration Release
 
 del "%~dp0/../src/Picturepark.SDK.V1.CloudManager/project.lock.json"
 dotnet restore "%~dp0/../src/Picturepark.SDK.V1.CloudManager/" --no-cache
-dotnet pack "%~dp0/../src/Picturepark.SDK.V1.CloudManager/" --output "../../build/Packages" --configuration Release
+dotnet pack "%~dp0/../src/Picturepark.SDK.V1.CloudManager/" --output "%~dp0/Packages" --configuration Release
