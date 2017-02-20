c2041db0984f5c5952987ed0fde8028a5747c5a9
Rico Suter <mail@rsuter.com>
2017-02-20 15:06:00 +0100

    Added xml docs to project.json

--- src/Picturepark.SDK.V1/Clients/ClientBase.cs
+++ src/Picturepark.SDK.V1/Clients/ClientBase.cs
@@ -6,16 +6,22 @@ using Picturepark.SDK.V1.Authentication;
 
 namespace Picturepark.SDK.V1
 {
-	public abstract class ClientBase
+    /// <summary>The base class for all clients.</summary>
+    public abstract class ClientBase
 	{
 		private readonly IAuthClient _authClient;
 
-	    protected ClientBase(IAuthClient configuration)
+        /// <summary>Initializes a new instance of the <see cref="ClientBase" /> class.</summary>
+        /// <param name="authClient">The authentication client.</param>
+        protected ClientBase(IAuthClient authClient)
 		{
-			_authClient = configuration;
+			_authClient = authClient;
 		}
 
-		protected async Task<HttpClient> CreateHttpClientAsync(CancellationToken ct)
+        /// <summary>Creates an HTTP client with a bearer authentication token from the <see cref="IAuthClient"/>.</summary>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>The HTTP client.</returns>
+        protected async Task<HttpClient> CreateHttpClientAsync(CancellationToken cancellationToken)
 		{
 			var client = new HttpClient();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
