5e94d352a6016627cdb60b62394c1b03ff8da298
Rico Suter <mail@rsuter.com>
2017-02-22 15:44:59 +0100

    Added Picturepark.AssetUploader (sample)

---
+++ src/Picturepark.AssetUploader/ViewModels/MainWindowModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using MyToolkit.Command;
+using MyToolkit.Dialogs;
+using MyToolkit.Mvvm;
+using MyToolkit.Storage;
+using Picturepark.SDK.V1;
+using Picturepark.SDK.V1.Authentication;
+using Picturepark.SDK.V1.Contract;
+
+namespace Picturepark.AssetUploader.ViewModels
+{
+    public class MainWindowModel : ViewModelBase
+    {
+        private string _filePath;
+
+        public MainWindowModel()
+        {
+            UploadCommand = new AsyncRelayCommand(UploadAsync, () =>
+                !string.IsNullOrEmpty(Server) &&
+                !string.IsNullOrEmpty(Username) &&
+                !string.IsNullOrEmpty(Password) &&
+                !string.IsNullOrEmpty(FilePath));
+
+            PropertyChanged += (sender, args) => UploadCommand.RaiseCanExecuteChanged();
+        }
+
+        public AsyncRelayCommand UploadCommand { get; }
+
+        public string Server
+        {
+            get { return ApplicationSettings.GetSetting("Server", ""); }
+            set { ApplicationSettings.SetSetting("Server", value); }
+        }
+
+        public string Username
+        {
+            get { return ApplicationSettings.GetSetting("Username", ""); }
+            set { ApplicationSettings.SetSetting("Username", value); }
+        }
+
+        public string Password
+        {
+            get { return ApplicationSettings.GetSetting("Password", ""); }
+            set { ApplicationSettings.SetSetting("Password", value); }
+        }
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set { Set(ref _filePath, value); }
+        }
+
+        private async Task UploadAsync()
+        {
+            if (File.Exists(FilePath))
+            {
+                var fileName = Path.GetFileName(FilePath);
+                await RunTaskAsync(async () =>
+                {
+                    var authClient = new UsernamePasswordAuthClient(Server, Username, Password);
+                    using (var client = new PictureparkClient(authClient))
+                    {
+                        var transfer = await client.Transfers.CreateAsync(new CreateTransferRequest
+                        {
+                            Name = fileName,
+                            TransferType = TransferType.FileUpload,
+                            Files = new List<TransferUploadFile> { new TransferUploadFile { FileName = fileName, Identifier = fileName } }
+                        });
+
+                        var fileSize = new FileInfo(FilePath);
+                        using (var stream = File.OpenRead(FilePath))
+                            await client.Transfers.UploadFileAsync(transfer.Id, fileName, new FileParameter(stream, fileName), fileName, 1, fileSize.Length, fileSize.Length, 1);
+                    }
+                });
+            }
+        }
+
+        public override void HandleException(Exception exception)
+        {
+            ExceptionBox.Show("An error occurred", exception, Application.Current.MainWindow);
+        }
+    }
+}
---
+++ src/Picturepark.AssetUploader/Views/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using Microsoft.Win32;
+using Picturepark.AssetUploader.ViewModels;
+
+namespace Picturepark.AssetUploader.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        public MainWindowModel Model => (MainWindowModel) Resources["ViewModel"];
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var args = Environment.GetCommandLineArgs();
+            if (args.Length == 2)
+                Model.FilePath = args[1];
+        }
+
+        private void OnRegisterInContextMenu(object sender, RoutedEventArgs e)
+        {
+            Model.RunTaskAsync(() =>
+            {
+                using (var key = Registry.ClassesRoot.CreateSubKey(@"*\shell\PictureparkAssetUploader"))
+                    key.SetValue("", "Upload to Picturepark server");
+
+                using (var key = Registry.ClassesRoot.CreateSubKey(@"*\shell\PictureparkAssetUploader\command"))
+                    key.SetValue("", "\"" + Assembly.GetEntryAssembly().Location + "\" %1");
+            });
+        }
+
+        private void OnUnregisterInContextMenu(object sender, RoutedEventArgs e)
+        {
+            Model.RunTaskAsync(() =>
+            {
+                Registry.ClassesRoot.DeleteSubKeyTree(@"*\shell\PictureparkAssetUploader");
+            });
+        }
+    }
+}
