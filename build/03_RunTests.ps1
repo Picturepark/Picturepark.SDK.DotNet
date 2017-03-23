83bf9af9c59929bcf37be16129f0caba34f8f861
Urs Brogle <ub@picturepark.com>
2017-03-23 11:19:33 +0100

    Try to fix tests.

--- build/03_RunTests.ps1
+++ build/03_RunTests.ps1
@@ -1,9 +1,9 @@
-if (!(Test-Path "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/Configuration.json")) { 
+if (!(Test-Path "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/bin/Configuration.json")) { 
 	(Get-Content "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/Configuration_template.json") | 
 	ForEach-Object { $_ -replace "{Server}", "$env:TestServer" } | 
 	ForEach-Object { $_ -replace "{Username}", "$env:TestUsername" } | 
 	ForEach-Object { $_ -replace "{Password}", "$env:TestPassword" } | 
-	Set-Content "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/Configuration.json"
+	Set-Content "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/bin/Configuration.json"
 }
 
 dotnet restore "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/" --no-cache
