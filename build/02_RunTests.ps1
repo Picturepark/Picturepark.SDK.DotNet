87c67dc6856d9b06e5ef08e419a4506c4421f117
Urs Brogle <ub@picturepark.com>
2017-08-30 10:30:21 +0200

    Added access token creation on test run.

--- build/02_RunTests.ps1
+++ build/02_RunTests.ps1
@@ -1,8 +1,21 @@
+$tokenParams = @{
+  client_id=${Env:TestIdentityClientId};
+  client_secret=${Env:TestIdentitySecret};
+  grant_type='password';
+  username=${Env:TestUsername};
+  password=${Env:TestPassword};
+}
+
+$result = Invoke-WebRequest ${Env:TestIdentityServer} -Method Post -Body $tokenParams | ConvertFrom-Json
+
+${Env:TestAccessToken} = $result.access_token
+
 if (!(Test-Path "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/Configuration.json")) { 
 	(Get-Content "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/Configuration_template.json") | 
 	ForEach-Object { $_ -replace "{Server}", "$env:TestServer" } | 
 	ForEach-Object { $_ -replace "{Username}", "$env:TestUsername" } | 
 	ForEach-Object { $_ -replace "{Password}", "$env:TestPassword" } | 
+	ForEach-Object { $_ -replace "{AccessToken}", "$env:TestAccessToken" } | 
 	Set-Content "$PSScriptRoot/../src/Picturepark.SDK.V1.Tests/Configuration.json"
 }
 
