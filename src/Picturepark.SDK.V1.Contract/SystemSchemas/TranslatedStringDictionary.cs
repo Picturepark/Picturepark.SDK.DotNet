69833611adb924e22611173f6290401d127274cb
Urs Brogle <ub@picturepark.com>
2017-10-14 12:17:57 +0200

    Added GetTranslation to TranslatedStringDictionary.

--- src/Picturepark.SDK.V1.Contract/SystemSchemas/TranslatedStringDictionary.cs
+++ src/Picturepark.SDK.V1.Contract/SystemSchemas/TranslatedStringDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using Picturepark.SDK.V1.Contract.Attributes;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace Picturepark.SDK.V1.Contract
 {
@@ -22,6 +23,17 @@ namespace Picturepark.SDK.V1.Contract
 
 			foreach (var item in dictionary)
 				Add(item.Key, item.Value);
+		}
+
+		/// <summary>
+		/// Get translation. Fallback to x-default if specified language does not exist
+		/// </summary>
+		/// <param name="twoLetterIsoLanguageName">Language to retrieve. Fallback to CultureInfo.CurrentCulture.TwoLetterISOLanguageName if not specified</param>
+		/// <returns></returns>
+		public string GetTranslation(string twoLetterIsoLanguageName = null)
+		{
+			var languageName = twoLetterIsoLanguageName ?? CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+			return ContainsKey(languageName) ? this[languageName] : this["x-default"];
 		}
 	}
 }
