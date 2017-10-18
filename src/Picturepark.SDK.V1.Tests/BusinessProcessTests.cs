5d1125b1c00e674438dace890d2b90f47421b510
Urs Brogle <ub@picturepark.com>
2017-10-18 08:31:41 +0200

    Updated NSwag to 11.10.0, regenerated clients. Added business process search test.

---
+++ src/Picturepark.SDK.V1.Tests/BusinessProcessTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Picturepark.SDK.V1.Contract;
+using Picturepark.SDK.V1.Tests.Fixtures;
+using Xunit;
+
+namespace Picturepark.SDK.V1.Tests
+{
+	public class BusinessProcessTests : IClassFixture<SDKClientFixture>
+	{
+		private readonly SDKClientFixture _fixture;
+		private readonly PictureparkClient _client;
+
+		public BusinessProcessTests(SDKClientFixture fixture)
+		{
+			_fixture = fixture;
+			_client = _fixture.Client;
+		}
+
+		[Fact]
+		[Trait("Stack", "Contents")]
+		public async Task ShouldSearchBusinessProcesses()
+		{
+			var results = await _client.BusinessProcesses.SearchAsync(new BusinessProcessSearchRequest
+			{
+				Start = 0,
+				Limit = 20,
+				Sort = new List<SortInfo>
+				{
+					new SortInfo
+					{
+						Field = "audit.creationDate",
+						Direction = SortDirection.Desc
+					}
+				}
+			});
+			Assert.NotNull(results);
+		}
+	}
+}
