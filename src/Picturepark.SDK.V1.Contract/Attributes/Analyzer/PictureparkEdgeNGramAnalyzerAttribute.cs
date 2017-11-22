e9e90fe9a88985897b47f5c4813ba4ae0a8bd6d6
Urs Brogle <ub@picturepark.com>
2017-11-22 20:29:27 +0100

    Added analyzer tests.

--- src/Picturepark.SDK.V1.Contract/Attributes/Analyzer/PictureparkEdgeNGramAnalyzerAttribute.cs
+++ src/Picturepark.SDK.V1.Contract/Attributes/Analyzer/PictureparkEdgeNGramAnalyzerAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace Picturepark.SDK.V1.Contract.Attributes.Analyzer
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
     public class PictureparkEdgeNGramAnalyzerAttribute : PictureparkAnalyzerAttribute
     {
         public override AnalyzerBase CreateAnalyzer()
--- src/Picturepark.SDK.V1.Contract/Attributes/Analyzer/PictureparkPathHierarchyAnalyzerAttribute.cs
+++ src/Picturepark.SDK.V1.Contract/Attributes/Analyzer/PictureparkPathHierarchyAnalyzerAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace Picturepark.SDK.V1.Contract.Attributes.Analyzer
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
     public class PictureparkPathHierarchyAnalyzerAttribute : PictureparkAnalyzerAttribute
     {
         /// <summary>Creates an analyzer based on the attribute.</summary>
