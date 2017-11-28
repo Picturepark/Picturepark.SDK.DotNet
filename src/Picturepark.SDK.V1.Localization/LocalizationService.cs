daa06e5380aeec360efd84c694bf9c8631ee7f81
Rico Suter <mail@rsuter.com>
2017-11-28 17:43:26 +0100

    Added LocalizedErrorTests

--- src/Picturepark.SDK.V1.Localization/LocalizationService.cs
+++ src/Picturepark.SDK.V1.Localization/LocalizationService.cs
@@ -5,93 +5,129 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Reflection;
 
 namespace Picturepark.SDK.V1.Localization
 {
+	/// <summary>Provides methods to translate texts from the Picturepark backend.</summary>
 	public class LocalizationService
 	{
 		private const string CatalogName = "picturepark";
 		private const string FallbackLanguage = "en";
 
-		private static readonly ConcurrentDictionary<string, ICatalog> s_CachedCatalogs = new ConcurrentDictionary<string, ICatalog>();
+		private static readonly ConcurrentDictionary<string, ICatalog> _cachedCatalogs = new ConcurrentDictionary<string, ICatalog>();
 
-		public static string ResolveErrorCode(PictureparkException exception, string language) // TODO: LocalizationService: Use language enum instead of string?
+		/// <summary>Gets the localized error message for the given exception.</summary>
+		/// <param name="exception">The exception.</param>
+		/// <param name="language">The language.</param>
+		/// <returns>The localized error message.</returns>
+		public static string GetLocalizedErrorCode(PictureparkException exception, string language)
 		{
 			var errorAsString = exception.GetType().Name;
-			return ResolveLocalizedText(errorAsString, Hash.FromAnonymousObject(exception), language);
+			return GetLocalizedText(errorAsString, language, Hash.FromAnonymousObject(exception));
 		}
 
-		public static string ResolveLocalizedText(int code, string language)
+		/// <summary>Gets the localized text for the given code and language.</summary>
+		/// <param name="code">The code to identify the text.</param>
+		/// <param name="language">The language.</param>
+		/// <returns>The localized text.</returns>
+		public static string GetLocalizedText(int code, string language)
 		{
-			return ResolveLocalizedText(code.ToString(), new Dictionary<string, object>(), language);
+			return GetLocalizedText(code.ToString(), new Dictionary<string, object>(), language);
 		}
 
-		public static string ResolveLocalizedText(int code, IDictionary<string, object> additionalData, string language)
+		/// <summary>Gets the localized text for the given code and language.</summary>
+		/// <param name="code">The code to identify the text.</param>
+		/// <param name="additionalData">The additional placeholder data to use in the text.</param>
+		/// <param name="language">The language.</param>
+		/// <returns>The localized text.</returns>
+		public static string GetLocalizedText(int code, IDictionary<string, object> additionalData, string language)
 		{
-			return ResolveLocalizedText(code.ToString(), additionalData, language);
+			return GetLocalizedText(code.ToString(), additionalData, language);
 		}
 
-		public static string ResolveLocalizedText(string code, string language)
+		/// <summary>Gets the localized text for the given code and language.</summary>
+		/// <param name="code">The code to identify the text.</param>
+		/// <param name="language">The language.</param>
+		/// <returns>The localized text.</returns>
+		public static string GetLocalizedText(string code, string language)
 		{
-			return ResolveLocalizedText(code, new Dictionary<string, object>(), language);
+			return GetLocalizedText(code, new Dictionary<string, object>(), language);
 		}
 
-		public static string ResolveLocalizedText(string code, IDictionary<string, object> additionalData, string language)
+		/// <summary>Gets the localized text for the given code and language.</summary>
+		/// <param name="code">The code to identify the text.</param>
+		/// <param name="additionalData">The additional placeholder data to use in the text.</param>
+		/// <param name="language">The language.</param>
+		/// <returns>The localized text.</returns>
+		public static string GetLocalizedText(string code, IDictionary<string, object> additionalData, string language)
 		{
-			return ResolveLocalizedText(code, Hash.FromDictionary(additionalData), language);
+			return GetLocalizedText(code, language, Hash.FromDictionary(additionalData));
 		}
 
-		private static string ResolveLocalizedText(string code, Hash additionalData, string language)
-		{
-			var cultureInfo = new CultureInfo(language);
-			var catalog = GetCatalog(language);
-
-			// catalog not found, so try to get the base catalog immediately
-			if (catalog == null)
-			{
-				cultureInfo = cultureInfo.Parent;
-				catalog = GetCatalog(cultureInfo.TwoLetterISOLanguageName);
-			}
-
-			// no catalog, just return the error code as a string
+		private static string GetLocalizedText(string code, string language, Hash additionalData)
+        {
+			var catalog = TryFindBestCatalog(language);
 			if (catalog == null)
 			{
 				return code;
 			}
 
-			var value = catalog.GetString(code);
-
-			// try on base catalog if no translation is found
-			if (!cultureInfo.IsNeutralCulture && value == code)
-			{
-				cultureInfo = cultureInfo.Parent;
-				catalog = GetCatalog(cultureInfo.TwoLetterISOLanguageName);
-				value = catalog.GetString(code);
-			}
-
-			// render data into translated string
+			var value = GetLocalizedRawText(catalog, code, language);
 			return Template.Parse(value).Render(additionalData);
 		}
 
-		private static ICatalog GetCatalog(string language)
+		private static string GetLocalizedRawText(ICatalog catalog, string code, string language)
+        {
+			var value = catalog.GetString(code);
+
+			var cultureInfo = new CultureInfo(language);
+			if (cultureInfo.IsNeutralCulture == false && value == code)
+			{
+				cultureInfo = cultureInfo.Parent;
+
+				catalog = TryGetCatalog(cultureInfo.TwoLetterISOLanguageName);
+				if (catalog != null)
+				{
+					return catalog.GetString(code);
+				}
+			}
+
+			return value;
+		}
+
+		private static ICatalog TryFindBestCatalog(string language)
+		{
+			var catalog = TryGetCatalog(language);
+			if (catalog != null)
+			{
+				return catalog;
+			}
+
+			var cultureInfo = new CultureInfo(language);
+			cultureInfo = cultureInfo.Parent;
+
+			return TryGetCatalog(cultureInfo.TwoLetterISOLanguageName);
+		}
+
+		private static ICatalog TryGetCatalog(string language)
 		{
 			ICatalog result;
 
-			if (!s_CachedCatalogs.TryGetValue(language, out result))
+			if (!_cachedCatalogs.TryGetValue(language, out result))
 			{
-				var cultureInfo = new CultureInfo(language);
+				var assembly = typeof(LocalizationService).GetTypeInfo().Assembly;
+				var resourceName = "Picturepark.SDK.V1.Localization.Languages." + language.ToLowerInvariant() + ".mo";
 
-				// TODO: Extend
-				// language == "de" ? Resources.de : Resources.en;
-				byte[] resourceStream = new byte[0];
-				try
+				using (Stream stream = assembly.GetManifestResourceStream(resourceName))
 				{
-					result = new Catalog(new MemoryStream(resourceStream), new CultureInfo(language));
-					s_CachedCatalogs.TryAdd(language, result);
-				}
-				catch
-				{
-					throw;
+					if (stream == null)
+					{
+						return null;
+					}
+
+					result = new Catalog(stream, new CultureInfo(language));
+					_cachedCatalogs.TryAdd(language, result);
 				}
 			}
 
---
+++ src/Picturepark.SDK.V1.Tests/Localization/LocalizationTests.cs
@@ -0,0 +1,45 @@
+﻿using Picturepark.SDK.V1.Contract;
+using Picturepark.SDK.V1.Localization;
+using Xunit;
+
+namespace Picturepark.SDK.V1.Tests.Localization
+{
+	public class LocalizationTests
+	{
+		[Fact]
+		[Trait("Stack", "Localization")]
+		public void ShouldLocalizeException()
+		{
+			/// Arrange
+			var exception = new CustomerNotFoundException();
+
+			/// Assert
+			var text = exception.GetLocalizedErrorCode("en");
+
+			/// Act
+			Assert.False(string.IsNullOrEmpty(text));
+		}
+
+		[Fact]
+		[Trait("Stack", "Localization")]
+		public void ShouldLocalizeTextFromStringCode()
+		{
+			/// Assert
+			var text = LocalizationService.GetLocalizedText("CustomerNotFoundException", "en");
+
+			/// Act
+			Assert.False(string.IsNullOrEmpty(text));
+		}
+
+		[Fact]
+		[Trait("Stack", "Localization")]
+		public void ShouldLocalizeTextFromIntegerCode()
+		{
+			/// Assert
+			var text = LocalizationService.GetLocalizedText(100304, "en");
+
+			/// Act
+			Assert.False(string.IsNullOrEmpty(text));
+		}
+	}
+}
