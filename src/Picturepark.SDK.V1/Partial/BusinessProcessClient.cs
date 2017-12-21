dcb2c19d353c8a26ea93e107ab67129a56b3dfe2
Rico Suter <mail@rsuter.com>
2017-12-21 12:12:47 +0100

    Added timeout extension to WaitForLifeCyclesAsync and WaitForStatesAsync

--- src/Picturepark.SDK.V1/Partial/BusinessProcessClient.cs
+++ src/Picturepark.SDK.V1/Partial/BusinessProcessClient.cs
@@ -7,139 +7,145 @@ using System.Threading.Tasks;
 
 namespace Picturepark.SDK.V1
 {
-	public partial class BusinessProcessClient
-	{
-		/// <summary>Waits until the business process transitioned into one of the given lifecycles or the timeout is reached.</summary>
-		/// <param name="processId">The process ID.</param>
-		/// <param name="lifeCycleIds">The business process lifecycle IDs to wait for.</param>
-		/// <param name="timeout">The timeout in ms to wait for completion.</param>
-		/// <returns>The wait result.</returns>
-		/// <exception cref="ApiException">A server side error occurred.</exception>
-		/// <exception cref="PictureparkException">Internal server error</exception>
-		public BusinessProcessWaitResult WaitForLifeCycles(string processId, IEnumerable<BusinessProcessLifeCycle> lifeCycleIds, TimeSpan? timeout = null)
-		{
-			return Task.Run(async () => await WaitForLifeCyclesAsync(processId, lifeCycleIds, timeout, CancellationToken.None).ConfigureAwait(false)).GetAwaiter().GetResult();
-		}
+    public partial class BusinessProcessClient
+    {
+        /// <summary>Waits until the business process transitioned into one of the given lifecycles or the timeout is reached.</summary>
+        /// <param name="processId">The process ID.</param>
+        /// <param name="lifeCycleIds">The business process lifecycle IDs to wait for.</param>
+        /// <param name="timeout">The timeout in ms to wait for completion.</param>
+        /// <returns>The wait result.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        /// <exception cref="PictureparkException">Internal server error</exception>
+        public BusinessProcessWaitResult WaitForLifeCycles(string processId, IEnumerable<BusinessProcessLifeCycle> lifeCycleIds, TimeSpan? timeout = null)
+        {
+            return Task.Run(async () => await WaitForLifeCyclesAsync(processId, lifeCycleIds, timeout, CancellationToken.None).ConfigureAwait(false)).GetAwaiter().GetResult();
+        }
 
-		/// <summary>Waits until the business process transitioned into one of the given lifecycles or the timeout is reached.</summary>
-		/// <param name="processId">The process ID.</param>
-		/// <param name="lifeCycleIds">The business process lifecycle IDs to wait for.</param>
-		/// <param name="timeout">The timeout in ms to wait for completion.</param>
-		/// <returns>The wait result.</returns>
-		/// <exception cref="ApiException">A server side error occurred.</exception>
-		/// <exception cref="PictureparkException">Internal server error</exception>
-		/// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-		public async Task<BusinessProcessWaitResult> WaitForLifeCyclesAsync(string processId, IEnumerable<BusinessProcessLifeCycle> lifeCycleIds, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
-		{
-			var waitResult = await WaitCoreAsync(processId, null, lifeCycleIds, timeout, cancellationToken).ConfigureAwait(false);
+        /// <summary>Waits until the business process transitioned into one of the given lifecycles or the timeout is reached.</summary>
+        /// <param name="processId">The process ID.</param>
+        /// <param name="lifeCycleIds">The business process lifecycle IDs to wait for.</param>
+        /// <param name="timeout">The timeout in ms to wait for completion.</param>
+        /// <returns>The wait result.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        /// <exception cref="PictureparkException">Internal server error</exception>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        public async Task<BusinessProcessWaitResult> WaitForLifeCyclesAsync(string processId, IEnumerable<BusinessProcessLifeCycle> lifeCycleIds, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await _httpClient.Poll(timeout, cancellationToken, async () =>
+            {
+                var waitResult = await WaitCoreAsync(processId, null, lifeCycleIds, timeout, cancellationToken).ConfigureAwait(false);
 
-			var errors = waitResult.BusinessProcess.StateHistory?
-				.Where(i => i.Error != null)
-				.Select(i => i.Error)
-				.ToList();
+                var errors = waitResult.BusinessProcess.StateHistory?
+                    .Where(i => i.Error != null)
+                    .Select(i => i.Error)
+                    .ToList();
 
-			if (errors != null && errors.Any())
-			{
-				if (errors.Count == 1)
-				{
-					throw PictureparkException.FromJson(errors.First().Exception);
-				}
+                if (errors != null && errors.Any())
+                {
+                    if (errors.Count == 1)
+                    {
+                        throw PictureparkException.FromJson(errors.First().Exception);
+                    }
 
-				var exceptions = errors.Select(error => PictureparkException.FromJson(error.Exception));
-				throw new AggregateException(exceptions);
-			}
+                    var exceptions = errors.Select(error => PictureparkException.FromJson(error.Exception));
+                    throw new AggregateException(exceptions);
+                }
 
-			return waitResult;
-		}
+                return waitResult;
+            });
+        }
 
-		/// <summary>Waits until the business process transitioned into one of the given states or the timeout is reached.</summary>
-		/// <param name="processId">The process ID.</param>
-		/// <param name="states">The states to wait for.</param>
-		/// <param name="timeout">The timeout in ms to wait for completion.</param>
-		/// <returns>The wait result.</returns>
-		/// <exception cref="ApiException">A server side error occurred.</exception>
-		/// <exception cref="PictureparkException">Internal server error</exception>
-		public BusinessProcessWaitResult WaitForStates(string processId, IEnumerable<string> states, TimeSpan? timeout = null)
-		{
-			return Task.Run(async () => await WaitForStatesAsync(processId, states, timeout, CancellationToken.None).ConfigureAwait(false)).GetAwaiter().GetResult();
-		}
+        /// <summary>Waits until the business process transitioned into one of the given states or the timeout is reached.</summary>
+        /// <param name="processId">The process ID.</param>
+        /// <param name="states">The states to wait for.</param>
+        /// <param name="timeout">The timeout in ms to wait for completion.</param>
+        /// <returns>The wait result.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        /// <exception cref="PictureparkException">Internal server error</exception>
+        public BusinessProcessWaitResult WaitForStates(string processId, IEnumerable<string> states, TimeSpan? timeout = null)
+        {
+            return Task.Run(async () => await WaitForStatesAsync(processId, states, timeout, CancellationToken.None).ConfigureAwait(false)).GetAwaiter().GetResult();
+        }
 
-		/// <summary>Waits until the business process transitioned into one of the given states or the timeout is reached.</summary>
-		/// <param name="processId">The process ID.</param>
-		/// <param name="states">The states to wait for.</param>
-		/// <param name="timeout">The timeout in ms to wait for completion.</param>
-		/// <returns>The wait result.</returns>
-		/// <exception cref="ApiException">A server side error occurred.</exception>
-		/// <exception cref="PictureparkException">Internal server error</exception>
-		/// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-		public async Task<BusinessProcessWaitResult> WaitForStatesAsync(string processId, IEnumerable<string> states, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
-		{
-			var waitResult = await WaitCoreAsync(processId, states, null, timeout, cancellationToken).ConfigureAwait(false);
+        /// <summary>Waits until the business process transitioned into one of the given states or the timeout is reached.</summary>
+        /// <param name="processId">The process ID.</param>
+        /// <param name="states">The states to wait for.</param>
+        /// <param name="timeout">The timeout in ms to wait for completion.</param>
+        /// <returns>The wait result.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        /// <exception cref="PictureparkException">Internal server error</exception>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        public async Task<BusinessProcessWaitResult> WaitForStatesAsync(string processId, IEnumerable<string> states, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await _httpClient.Poll(timeout, cancellationToken, async () =>
+            {
+                var waitResult = await WaitCoreAsync(processId, states, null, timeout, cancellationToken).ConfigureAwait(false);
 
-			var errors = waitResult.BusinessProcess.StateHistory?
-				.Where(i => i.Error != null)
-				.Select(i => i.Error)
-				.ToList();
+                var errors = waitResult.BusinessProcess.StateHistory?
+                    .Where(i => i.Error != null)
+                    .Select(i => i.Error)
+                    .ToList();
 
-			if (errors != null && errors.Any())
-			{
-				if (errors.Count == 1)
-				{
-					throw PictureparkException.FromJson(errors.First().Exception);
-				}
+                if (errors != null && errors.Any())
+                {
+                    if (errors.Count == 1)
+                    {
+                        throw PictureparkException.FromJson(errors.First().Exception);
+                    }
 
-				var exceptions = errors.Select(error => PictureparkException.FromJson(error.Exception));
-				throw new AggregateException(exceptions);
-			}
+                    var exceptions = errors.Select(error => PictureparkException.FromJson(error.Exception));
+                    throw new AggregateException(exceptions);
+                }
 
-			return waitResult;
-		}
+                return waitResult;
+            });
+        }
 
-		/// <summary>Waits until the business process is completed.</summary>
-		/// <param name="processId">The process ID.</param>
-		/// <param name="timeout">The timeout wait for completion.</param>
-		/// /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-		/// <returns>The wait result.</returns>
-		/// <exception cref="ApiException">A server side error occurred.</exception>
-		/// <exception cref="PictureparkException">Internal server error</exception>
-		public BusinessProcessWaitResult WaitForCompletion(string processId, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
-		{
-			return Task.Run(async () => await WaitForCompletionCoreAsync(processId, timeout, cancellationToken).ConfigureAwait(false)).GetAwaiter().GetResult();
-		}
+        /// <summary>Waits until the business process is completed.</summary>
+        /// <param name="processId">The process ID.</param>
+        /// <param name="timeout">The timeout wait for completion.</param>
+        /// /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>The wait result.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        /// <exception cref="PictureparkException">Internal server error</exception>
+        public BusinessProcessWaitResult WaitForCompletion(string processId, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.Run(async () => await WaitForCompletionAsync(processId, timeout, cancellationToken).ConfigureAwait(false)).GetAwaiter().GetResult();
+        }
 
-		/// <summary>Waits until the business process is completed or the timeout is reached.</summary>
-		/// <param name="processId">The process ID.</param>
-		/// <param name="timeout">The timeout wait for completion.</param>
-		/// <returns>The wait result.</returns>
-		/// <exception cref="ApiException">A server side error occurred.</exception>
-		/// <exception cref="PictureparkException">Internal server error</exception>
-		/// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-		public async Task<BusinessProcessWaitResult> WaitForCompletionAsync(string processId, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
-		{
-			return await _httpClient.Poll(timeout, cancellationToken, async () =>
-			{
-				var waitResult = await WaitForCompletionCoreAsync(processId, timeout, cancellationToken).ConfigureAwait(false);
-				if (waitResult.HasLifeCycleHit && waitResult.BusinessProcess.LifeCycle == BusinessProcessLifeCycle.Succeeded)
-					return waitResult;
+        /// <summary>Waits until the business process is completed or the timeout is reached.</summary>
+        /// <param name="processId">The process ID.</param>
+        /// <param name="timeout">The timeout wait for completion.</param>
+        /// <returns>The wait result.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        /// <exception cref="PictureparkException">Internal server error</exception>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        public async Task<BusinessProcessWaitResult> WaitForCompletionAsync(string processId, TimeSpan? timeout = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return await _httpClient.Poll(timeout, cancellationToken, async () =>
+            {
+                var waitResult = await WaitForCompletionCoreAsync(processId, timeout, cancellationToken).ConfigureAwait(false);
+                if (waitResult.HasLifeCycleHit && waitResult.BusinessProcess.LifeCycle == BusinessProcessLifeCycle.Succeeded)
+                    return waitResult;
 
-				var errors = waitResult.BusinessProcess.StateHistory?
-					.Where(i => i.Error != null)
-					.Select(i => i.Error)
-					.ToList();
+                var errors = waitResult.BusinessProcess.StateHistory?
+                    .Where(i => i.Error != null)
+                    .Select(i => i.Error)
+                    .ToList();
 
-				if (errors != null && errors.Any())
-				{
-					if (errors.Count == 1)
-					{
-						throw PictureparkException.FromJson(errors.First().Exception);
-					}
+                if (errors != null && errors.Any())
+                {
+                    if (errors.Count == 1)
+                    {
+                        throw PictureparkException.FromJson(errors.First().Exception);
+                    }
 
-					var exceptions = errors.Select(error => PictureparkException.FromJson(error.Exception));
-					throw new AggregateException(exceptions);
-				}
+                    var exceptions = errors.Select(error => PictureparkException.FromJson(error.Exception));
+                    throw new AggregateException(exceptions);
+                }
 
-				throw new TimeoutException($"Wait for business process on completion timed out after {timeout?.TotalSeconds} seconds");
-			});
-		}
-	}
+                throw new TimeoutException($"Wait for business process on completion timed out after {timeout?.TotalSeconds} seconds");
+            });
+        }
+    }
 }
