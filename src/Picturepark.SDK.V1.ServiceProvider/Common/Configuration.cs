7415a48634a5d2386f75b48a075c0e9f0967bd2f
VIT\s.bader <sb@picturepark.com>
2018-02-02 16:16:15 +0100

    PP9-5235, add SSL support

--- src/Picturepark.SDK.V1.ServiceProvider/Common/Configuration.cs
+++ src/Picturepark.SDK.V1.ServiceProvider/Common/Configuration.cs
@@ -2,31 +2,33 @@
 
 namespace Picturepark.SDK.V1.ServiceProvider
 {
-	public class Configuration
-	{
-		public Configuration()
-		{
-			SerializerSettings = new JsonSerializerSettings();
-			SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter { CamelCaseText = false });
-			SerializerSettings.Culture = System.Globalization.CultureInfo.InvariantCulture;
-			SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-			SerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
-		}
+    public class Configuration
+    {
+        public Configuration()
+        {
+            SerializerSettings = new JsonSerializerSettings();
+            SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter { CamelCaseText = false });
+            SerializerSettings.Culture = System.Globalization.CultureInfo.InvariantCulture;
+            SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            SerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
+        }
 
-		public string Port { get; set; }
+        public string Port { get; set; }
 
-		public string Host { get; set; }
+        public string Host { get; set; }
 
-		public string User { get; set; }
+        public string User { get; set; }
 
-		public string Password { get; set; }
+        public string Password { get; set; }
 
-		public string ServiceProviderId { get; set; }
+        public bool UseSsl { get; set; } = true;
 
-		public string NodeId { get; set; }
+        public string ServiceProviderId { get; set; }
 
-		public JsonSerializerSettings SerializerSettings { get; set; }
+        public string NodeId { get; set; }
 
-		public int DefaultQueuePriorityMax { get; set; } = 200;
-	}
+        public JsonSerializerSettings SerializerSettings { get; set; }
+
+        public int DefaultQueuePriorityMax { get; set; } = 200;
+    }
 }
