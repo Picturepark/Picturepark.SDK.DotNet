9e59909a259fc758fd517c82d7a7a227a4518b1c
Rico Suter <mail@rsuter.com>
2018-02-07 16:30:44 +0100

    Added FileResponseExtensions.GetFileName method

---
+++ src/Picturepark.SDK.V1/FileResponseExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Net;
+using Microsoft.Net.Http.Headers;
+using Picturepark.SDK.V1.Contract;
+
+namespace Picturepark.SDK.V1
+{
+    public static class FileResponseExtensions
+    {
+        public static string GetFileName(this FileResponse response)
+        {
+            var disposition = response.Headers["Content-Disposition"].FirstOrDefault();
+            if (disposition != null)
+            {
+                var composition = ContentDispositionHeaderValue.Parse(disposition);
+                if (composition != null)
+                {
+                    return WebUtility.UrlDecode(HeaderUtilities.RemoveQuotes(composition.FileName));
+                }
+            }
+
+            return null;
+        }
+    }
+}
