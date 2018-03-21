d3d105132db2ca1fb082e3bc58bb861394961618
Urs Brogle <ub@picturepark.com>
2018-03-21 14:43:38 +0100

    Fixed DescriptionTranslationAttribute. PP9-5526

--- src/Picturepark.SDK.V1.Contract/Attributes/PictureparkDescriptionTranslationAttribute.cs
+++ src/Picturepark.SDK.V1.Contract/Attributes/PictureparkDescriptionTranslationAttribute.cs
@@ -2,22 +2,22 @@
 
 namespace Picturepark.SDK.V1.Contract.Attributes
 {
-	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = true)]
-	public class PictureparkDescriptionTranslationAttribute : Attribute, IPictureparkAttribute
-	{
-	    public PictureparkDescriptionTranslationAttribute(string translation)
-	    {
-	        Translation = translation;
-	    }
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = true)]
+    public class PictureparkDescriptionTranslationAttribute : Attribute, IPictureparkAttribute
+    {
+        public PictureparkDescriptionTranslationAttribute(string translation)
+        {
+            Translation = translation;
+        }
 
-	    public PictureparkDescriptionTranslationAttribute(string languageAbbreviation, string translation)
-		{
-			LanguageAbbreviation = languageAbbreviation;
-			Translation = translation;
-		}
+        public PictureparkDescriptionTranslationAttribute(string languageAbbreviation, string translation)
+        {
+            LanguageAbbreviation = languageAbbreviation;
+            Translation = translation;
+        }
 
-		public string LanguageAbbreviation { get; }
+        public string LanguageAbbreviation { get; }
 
-		public string Translation { get; }
-	}
+        public string Translation { get; }
+    }
 }
