6c1fdf33abe9c689978a9a3d15546058e04de020
AD\s.belotti <s.belotti@picturepark.com>
2018-06-19 15:52:04 +0200

    PP9-5900: Contracts and client regenerated

--- src/Picturepark.SDK.V1.Contract/Contracts.Generated.cs
+++ src/Picturepark.SDK.V1.Contract/Contracts.Generated.cs
@@ -2962,8 +2962,8 @@ namespace Picturepark.SDK.V1.Contract
     [Newtonsoft.Json.JsonObjectAttribute]
     public partial class PermissionSetNotFoundException : PictureparkNotFoundException
     {
-        [Newtonsoft.Json.JsonProperty("permissionSetId", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
-        public string PermissionSetId { get; set; }
+        [Newtonsoft.Json.JsonProperty("permissionSetIds", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public System.Collections.Generic.ICollection<string> PermissionSetIds { get; set; }
     
         public string ToJson() 
         {
