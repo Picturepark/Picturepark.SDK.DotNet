d4ca67779378ee557178fd125090660384cd6264
Marek Sajna <ms@picturepark.com>
2019-03-26 14:37:06 +0100

    PP9-7818: Fixing the outdated customer create json in CloudManager tests

--- src/Picturepark.SDK.V1.Contract/Extensions/MiscExtensions.cs
+++ src/Picturepark.SDK.V1.Contract/Extensions/MiscExtensions.cs
@@ -7,8 +7,8 @@ namespace Picturepark.SDK.V1.Contract.Extensions
         public static string JoinByDot(this (string a, string b) self)
         {
             return self.a != null
-                ? (self.b != null ? $"{self.a}.{self.b}" : self.a)
-                : (self.b != null ? self.b : throw new Exception("unable to join two nulls"));
+                ? self.b != null ? $"{self.a}.{self.b}" : self.a
+                : self.b ?? throw new Exception("unable to join two nulls");
         }
     }
 }
