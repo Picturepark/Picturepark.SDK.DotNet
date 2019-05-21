79af55e3fd7d3a7f12e7e59b82167b85d014fb26
Marek Sajna <ms@picturepark.com>
2019-05-21 20:37:07 +0200

    PP9-7781: Adding tests for new user role endpoints

--- src/Picturepark.SDK.V1.Tests/Clients/UserRoleTests.cs
+++ src/Picturepark.SDK.V1.Tests/Clients/UserRoleTests.cs
@@ -12,6 +12,8 @@ namespace Picturepark.SDK.V1.Tests.Clients
     {
         private const string JamesBond = "James Bond";
         private const string PeterGriffin = "Peter Griffin";
+        private const string UpdatedRoleName = "Updated Role";
+        private const string UpdatedRoleName2 = "Updated Role 2";
 
         private readonly IPictureparkService _client;
 
@@ -49,11 +51,83 @@ namespace Picturepark.SDK.V1.Tests.Clients
             }
         }
 
+        [Fact]
+        [Trait("Stack", "UserRoles")]
+        public async Task ShouldUpdateSingle()
+        {
+            // Arrange
+            var roles = await CreateRoles().ConfigureAwait(false);
+
+            try
+            {
+                var role = roles.First();
+                var update = new UserRoleEditable
+                {
+                    Names = role.Names,
+                    UserRights = role.UserRights
+                };
+                update.Names["en"] = UpdatedRoleName;
+
+                // Act
+                var updatedRole = await _client.UserRole.UpdateAsync(role.Id, update).ConfigureAwait(false);
+
+                // Assert
+                updatedRole.Names.Should().ContainKey("en").And.Subject["en"].Should().Be(UpdatedRoleName);
+                updatedRole.UserRights.Should().BeEquivalentTo(role.UserRights);
+
+                var updateRoleFromApi = await _client.UserRole.GetAsync(role.Id).ConfigureAwait(false);
+                updateRoleFromApi.Names.Should().ContainKey("en").And.Subject["en"].Should().Be(UpdatedRoleName);
+                updateRoleFromApi.UserRights.Should().BeEquivalentTo(role.UserRights);
+            }
+            finally
+            {
+                await Delete(roles).ConfigureAwait(false);
+            }
+        }
+
+        [Fact]
+        [Trait("Stack", "UserRoles")]
+        public async Task ShouldUpdateAndGetMany()
+        {
+            // Arrange
+            var roles = await CreateRoles().ConfigureAwait(false);
+
+            try
+            {
+                var role1 = roles[0];
+                var role2 = roles[1];
+                var update = new UserRoleUpdateManyRequest
+                {
+                    Items = new[] { role1, role2 }
+                };
+                role1.Names["en"] = UpdatedRoleName;
+                role2.Names["en"] = UpdatedRoleName2;
+
+                // Act
+                var bulkResponse = await _client.UserRole.UpdateManyAsync(update).ConfigureAwait(false);
+
+                // Assert
+                bulkResponse.Rows.Should().OnlyContain(r => r.Succeeded);
+
+                var updatedRoles = (await _client.UserRole.GetManyAsync(update.Items.Select(u => u.Id)).ConfigureAwait(false)).ToDictionary(u => u.Id);
+
+                updatedRoles[role1.Id].Names.Should().ContainKey("en").And.Subject["en"].Should().Be(UpdatedRoleName);
+                updatedRoles[role1.Id].UserRights.Should().BeEquivalentTo(role1.UserRights);
+
+                updatedRoles[role2.Id].Names.Should().ContainKey("en").And.Subject["en"].Should().Be(UpdatedRoleName2);
+                updatedRoles[role2.Id].UserRights.Should().BeEquivalentTo(role2.UserRights);
+            }
+            finally
+            {
+                await Delete(roles).ConfigureAwait(false);
+            }
+        }
+
         private async Task Delete(IEnumerable<UserRole> roles)
         {
             await _client.UserRole.DeleteManyAsync(new UserRoleDeleteManyRequest
             {
-                Ids = roles.Select(r => r.Id).ToArray()
+                Items = roles.Select(r => r.Id).ToArray()
             });
         }
 
