82f005ed705c5aa2ddb0d1a2facf6c861cafe65b
Marek Sajna <ms@picturepark.com>
2019-05-22 09:51:30 +0200

    PP9-7781: Adding GetAll alias for UserRoles

---
+++ src/Picturepark.SDK.V1/Partial/UserRoleClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Picturepark.SDK.V1.Contract;
+
+namespace Picturepark.SDK.V1
+{
+    public partial class UserRoleClient
+    {
+        public async Task<ICollection<UserRole>> GetAllAsync(CancellationToken cancellationToken = default(CancellationToken))
+            => await GetManyAsync(cancellationToken: cancellationToken);
+    }
+}
