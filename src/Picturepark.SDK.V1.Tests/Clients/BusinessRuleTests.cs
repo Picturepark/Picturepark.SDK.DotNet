9f1d25a8f86d2aa42d44cdb55834ab23d750b11d
Christian Güdel <cg@picturepark.com>
2019-07-15 18:45:12 +0200

    PP9-8694: add client + simple test as an example

---
+++ src/Picturepark.SDK.V1.Tests/Clients/BusinessRuleTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Picturepark.SDK.V1.Contract;
+using Picturepark.SDK.V1.Tests.Fixtures;
+using Xunit;
+
+namespace Picturepark.SDK.V1.Tests.Clients
+{
+    public class BusinessRuleTests : IClassFixture<ClientFixture>
+    {
+        private readonly IPictureparkService _client;
+
+        public BusinessRuleTests(ClientFixture fixture)
+        {
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        [Trait("Stack", "BusinessRule")]
+        public async Task ShouldStoreAndRetrieveConfiguration()
+        {
+            var rules = new List<BusinessRule>
+            {
+                new BusinessRuleConfigurable
+                {
+                    Id = $"{Guid.NewGuid():N}",
+                    Names = new TranslatedStringDictionary
+                    {
+                        { "en", "A sample rule" }
+                    },
+                    Description = new TranslatedStringDictionary
+                    {
+                        { "en", "Assign a permission set upon uploading of any image or video" }
+                    },
+                    IsEnabled = true,
+                    TriggerPoint = new BusinessRuleTriggerPoint
+                    {
+                        DocumentType = BusinessRuleTriggerDocType.Content,
+                        ExecutionScope = BusinessRuleExecutionScope.MainDoc,
+                        Action = BusinessRuleTriggerAction.Create
+                    },
+                    Condition = new OrCondition
+                    {
+                        Conditions = new List<BusinessRuleCondition>
+                        {
+                            new ContentSchemaCondition { SchemaId = "ImageMetadata" },
+                            new ContentSchemaCondition { SchemaId = "VideoMetadata" }
+                        }
+                    },
+                    Actions = new List<BusinessRuleAction>
+                    {
+                        new AssignContentPermissionSetsAction
+                        {
+                            PermissionSetIds = new[] { $"{Guid.NewGuid():N}" }
+                        }
+                    }
+                }
+            };
+
+            // in order not to interfere with other tests, we intentionally disable the rule engine here
+            var businessProcess = await _client.BusinessRule.UpdateConfigurationAsync(true, rules).ConfigureAwait(false);
+            await _client.BusinessProcess.WaitForCompletionAsync(businessProcess.Id).ConfigureAwait(false);
+
+            // retrieve config again
+            var storedConfiguration = await _client.BusinessRule.GetConfigurationAsync().ConfigureAwait(false);
+            storedConfiguration.DisableRuleEngine.Should().BeTrue();
+            storedConfiguration.Rules.Should().HaveCount(1);
+        }
+    }
+}
