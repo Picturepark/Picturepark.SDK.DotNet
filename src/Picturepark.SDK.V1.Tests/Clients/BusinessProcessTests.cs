770c59fec80d14d5feb5f3b393cb845e067e47e1
Christian Güdel <cg@picturepark.com>
2019-08-14 12:24:15 +0200

    PP9-8910: regenerate SDK, add tests for externally driven business processes

--- src/Picturepark.SDK.V1.Tests/Clients/BusinessProcessTests.cs
+++ src/Picturepark.SDK.V1.Tests/Clients/BusinessProcessTests.cs
@@ -6,10 +6,13 @@ using Picturepark.SDK.V1.Tests.Contracts;
 using Picturepark.SDK.V1.Tests.Fixtures;
 using Xunit;
 using System.Linq;
+using FluentAssertions;
+
 #pragma warning disable 1587
 
 namespace Picturepark.SDK.V1.Tests.Clients
 {
+    [Trait("Stack", "BusinessProcesses")]
     public class BusinessProcessTests : IClassFixture<ClientFixture>
     {
         private readonly IPictureparkService _client;
@@ -20,7 +23,6 @@ namespace Picturepark.SDK.V1.Tests.Clients
         }
 
         [Fact]
-        [Trait("Stack", "BusinessProcesses")]
         public async Task ShouldSearchBusinessProcesses()
         {
             // Arrange
@@ -38,7 +40,6 @@ namespace Picturepark.SDK.V1.Tests.Clients
         }
 
         [Fact]
-        [Trait("Stack", "BusinessProcesses")]
         public async Task ShouldGetBusinessProcessDetails()
         {
             // Arrange
@@ -88,6 +89,114 @@ namespace Picturepark.SDK.V1.Tests.Clients
 
             // Assert
             Assert.True(details.LifeCycle == BusinessProcessLifeCycle.Succeeded);
+        }
+
+        [Fact]
+        public async Task Should_create_external_business_process()
+        {
+            var businessProcess = await CreateBusinessProcess().ConfigureAwait(false);
+
+            businessProcess.SupportsCancellation.Should().BeFalse();
+            businessProcess.CurrentState.Should().Be("Started");
+        }
+
+        [Fact]
+        public async Task Should_change_business_process_state()
+        {
+            var businessProcess = await CreateBusinessProcess().ConfigureAwait(false);
+
+            var updated = await _client.BusinessProcess.ChangeStateAsync(
+                businessProcess.Id,
+                new BusinessProcessStateChangeRequest
+                {
+                    State = "Intermediate",
+                    LifeCycle = BusinessProcessLifeCycle.InProgress
+                }).ConfigureAwait(false);
+
+            updated.CurrentState.Should().Be("Intermediate");
+            updated.LifeCycle.Should().Be(BusinessProcessLifeCycle.InProgress);
+            updated.StateHistory.Should().Contain(x => x.State == "Intermediate");
+        }
+
+        [Fact]
+        public async Task Should_post_notification_update()
+        {
+            var businessProcess = await CreateBusinessProcess().ConfigureAwait(false);
+
+            // passes when no exception is risen
+            await _client.BusinessProcess.UpdateNotificationAsync(
+                businessProcess.Id,
+                new BusinessProcessNotificationUpdateRequest
+                {
+                    Title = new TranslatedStringDictionary
+                    {
+                        { "en", "Title updated" },
+                        { "de", "Aktualisierter Titel" }
+                    },
+                    Message = new TranslatedStringDictionary
+                    {
+                        { "en", "Message updated" },
+                        { "de", "Aktualisierte Nachricht" }
+                    },
+                    EventType = NotificationEventType.Warning
+                }).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task Should_cancel_running_business_process()
+        {
+            var businessProcess = await CreateBusinessProcess().ConfigureAwait(false);
+
+            await _client.BusinessProcess.CancelAsync(businessProcess.Id).ConfigureAwait(false);
+
+            businessProcess = await _client.BusinessProcess.GetAsync(businessProcess.Id).ConfigureAwait(false);
+            businessProcess.LifeCycle.Should().Be(BusinessProcessLifeCycle.CancellationInProgress);
+
+            businessProcess = await _client.BusinessProcess.ChangeStateAsync(
+                businessProcess.Id,
+                new BusinessProcessStateChangeRequest
+                {
+                    LifeCycle = BusinessProcessLifeCycle.Cancelled,
+                    State = "Cancelled"
+                }).ConfigureAwait(false);
+
+            businessProcess.CurrentState.Should().Be("Cancelled");
+            businessProcess.LifeCycle.Should().Be(BusinessProcessLifeCycle.Cancelled);
+        }
+
+        [Fact]
+        public async Task Should_get_business_process()
+        {
+            var businessProcess = await CreateBusinessProcess().ConfigureAwait(false);
+
+            var retrieved = await _client.BusinessProcess.GetAsync(businessProcess.Id).ConfigureAwait(false);
+            retrieved.Id.Should().Be(businessProcess.Id);
+        }
+
+        private async Task<BusinessProcess> CreateBusinessProcess()
+        {
+            var businessProcess = await _client.BusinessProcess.CreateAsync(
+                new BusinessProcessCreateRequest
+                {
+                    InitialState = "Started",
+                    SupportsCancellation = false,
+                    Notification = new BusinessProcessNotificationUpdate
+                    {
+                        EventType = NotificationEventType.InProgress,
+                        Message = new TranslatedStringDictionary
+                        {
+                            { "en", "Message" },
+                            { "de", "Nachricht" }
+                        },
+                        Title = new TranslatedStringDictionary
+                        {
+                            { "en", "Title" },
+                            { "de", "Titel" }
+                        }
+                    }
+                }).ConfigureAwait(false);
+
+            return businessProcess;
         }
     }
 }
