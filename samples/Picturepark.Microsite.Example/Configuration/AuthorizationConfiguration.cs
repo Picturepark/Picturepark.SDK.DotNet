637ccdfa1462b580019c58aa170abaa06aadd9c4
Marek Sajna <ms@picturepark.com>
2019-10-07 16:27:59 +0200

    Removing old microsite sample

--- samples/Picturepark.Microsite.Example/Configuration/AuthorizationConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Picturepark.Microsite.Example.Configuration
-{
-    public class AuthorizationConfiguration
-    {
-        public string[] AutoAssignUserRoleIds { get; set; }
-    }
-}
