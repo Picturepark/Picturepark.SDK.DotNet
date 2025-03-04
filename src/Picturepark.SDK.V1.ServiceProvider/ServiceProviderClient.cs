c5b9ee619aa3cb8f08336ba9586fc50260ae9e80
Ivan Yochev <vani172@gmail.com>
2025-03-04 11:47:25 +0200

    PP9-20679 clean up service provider client

--- src/Picturepark.SDK.V1.ServiceProvider/ServiceProviderClient.cs
+++ src/Picturepark.SDK.V1.ServiceProvider/ServiceProviderClient.cs
@@ -21,7 +21,6 @@ namespace Picturepark.SDK.V1.ServiceProvider
         private readonly Configuration _configuration;
         private readonly ConnectionFactory _factory;
         private readonly LiveStreamBuffer _liveStreamBuffer;
-        private readonly Task _initializeTask;
 
         private IConnection _connection;
         private IChannel _liveStreamChannel;
@@ -35,7 +34,6 @@ namespace Picturepark.SDK.V1.ServiceProvider
 
             _factory = CreateConnectionFactory(configuration);
 
-            _initializeTask = ConnectAsync(CancellationToken.None);
             _liveStreamBuffer = new LiveStreamBuffer(new LiveStreamBufferPriorityQueue());
             _liveStreamBuffer.Start();
         }
@@ -48,22 +46,15 @@ namespace Picturepark.SDK.V1.ServiceProvider
             await _connection.CloseAsync();
         }
 
-        public async Task<IObservable<EventPattern<EventArgsLiveStreamMessage>>> GetLiveStreamObserver(int bufferSize = 0, int delayMilliseconds = 0, CancellationToken cancellationToken = default)
+        public async Task<IObservable<EventPattern<EventArgsLiveStreamMessage>>> GetLiveStreamObserver(
+            int bufferSize = 0, int delayMilliseconds = 0, CancellationToken cancellationToken = default)
         {
-            await _initializeTask; // maybe better to start the task here instead, and pass the cancellation token
-
             // buffer
             _liveStreamBuffer.BufferHoldBackTimeMilliseconds = delayMilliseconds;
 
-#pragma warning disable CS0618 // Type or member is obsolete
-            var queueName = $"{DefaultExchangeName}.{_configuration.NodeId}";
-#pragma warning restore CS0618 // Type or member is obsolete
-            var isUnprotectedProvider = await TryDeclareExchangeAndBindQueue(queueName, cancellationToken);
-            if (!isUnprotectedProvider)
-            {
-                await ConnectAsync(cancellationToken);
-                queueName = DefaultQueueName;
-            }
+            _connection = await _factory.CreateConnectionAsync(cancellationToken);
+            _liveStreamChannel = await _connection.CreateChannelAsync(cancellationToken: cancellationToken);
+            _requestMessageChannel = await _connection.CreateChannelAsync(cancellationToken: cancellationToken);
 
             await _liveStreamChannel.BasicQosAsync(0, (ushort)bufferSize, false, cancellationToken);
 
@@ -84,35 +75,9 @@ namespace Picturepark.SDK.V1.ServiceProvider
                 _liveStreamConsumer.OnReceived(o, e);
                 return Task.CompletedTask;
             };
-            await _liveStreamChannel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer, cancellationToken);
+            await _liveStreamChannel.BasicConsumeAsync(queue: DefaultQueueName, autoAck: false, consumer: consumer, cancellationToken);
 
             return result;
-        }
-
-        private async Task<bool> TryDeclareExchangeAndBindQueue(string queueName, CancellationToken cancellationToken)
-        {
-            try
-            {
-                await _liveStreamChannel.ExchangeDeclareAsync(DefaultExchangeName, ExchangeType.Fanout, cancellationToken: cancellationToken);
-
-                var args = new Dictionary<string, object> { { "x-max-priority", _configuration.DefaultQueuePriorityMax } };
-
-                // queue
-                var queueDeclareOk = await _liveStreamChannel.QueueDeclareAsync(queueName, true, false, false, args, cancellationToken: cancellationToken);
-                await _liveStreamChannel.QueueBindAsync(queueDeclareOk, DefaultExchangeName, string.Empty, null, cancellationToken: cancellationToken);
-                return true;
-            }
-            catch (OperationInterruptedException ex) when (ex.ShutdownReason.ReplyCode == 403)
-            {
-                return false;
-            }
-        }
-
-        private async Task ConnectAsync(CancellationToken cancellationToken)
-        {
-            _connection = await _factory.CreateConnectionAsync(cancellationToken);
-            _liveStreamChannel = await _connection.CreateChannelAsync(cancellationToken: cancellationToken);
-            _requestMessageChannel = await _connection.CreateChannelAsync(cancellationToken: cancellationToken);
         }
 
         private ConnectionFactory CreateConnectionFactory(Configuration configuration)
@@ -135,7 +100,8 @@ namespace Picturepark.SDK.V1.ServiceProvider
                     Version = SslProtocols.Tls12,
                     Enabled = true,
                     ServerName = factory.HostName,
-                    AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch | SslPolicyErrors.RemoteCertificateChainErrors
+                    AcceptablePolicyErrors = SslPolicyErrors.RemoteCertificateNameMismatch |
+                                             SslPolicyErrors.RemoteCertificateChainErrors
                 };
             }
 
