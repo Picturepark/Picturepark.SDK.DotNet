0609585262cb2e01dd47d86238420e2deddebd1a
Ivan Yochev <vani172@gmail.com>
2025-08-25 16:24:03 +0300

    PP9-21783 Rebranding in SDK Docs

--- src/Picturepark.SDK.V1.Contract/Attributes/IPictureparkAttribute.cs
+++ src/Picturepark.SDK.V1.Contract/Attributes/IPictureparkAttribute.cs
@@ -1,6 +1,6 @@
 ﻿namespace Picturepark.SDK.V1.Contract.Attributes
 {
-    /// <summary>Marker interface for Picturepark specific .NET attributes.</summary>
+    /// <summary>Marker interface for Fotoware Alto specific .NET attributes.</summary>
     public interface IPictureparkAttribute
     {
     }
